efa100fac40bdde3e6ae326931bc45f42afeabc6
蔡凯升 <caikaisheng@wepie.com>
2023-04-03 14:49:17 +0800

    feat: 添加gitignore

--- test/main.go
+++ test/main.go
@@ -2,14 +2,31 @@ package main
 
 import "fmt"
 
-type myStruct struct {
-	ee int
-}
+// type person struct {
+// 	name string
+// 	age  int
+// }
+
+// func main() {
+// 	// 定义一个 person 变量 a
+// 	a := person{name: "Tom", age: 20}
+
+// 	// 赋值操作 b = a
+// 	b := a
+// 	b.age = 30
+// 	fmt.Println(a, b, &a, &b)
+// 	fmt.Printf("a: %v\n", &a)
+
+// 	// // 赋值操作 b = &a
+// 	// c := &a
+// 	// c.age = 40
+// 	// fmt.Printf("a: %v, c: %v\n", a, *c)
+// }
 
 func main() {
-	var a myStruct = myStruct{}
-	var b *myStruct = &a
-	a.ee = 2
-	fmt.Println(a.ee, (*b).ee, &a, a, &b)
-	// fmt.Println(*a)
+	a := []int{1, 2, 3}
+	b := a
+	b[0] = 4
+	fmt.Println(a, b)
+	fmt.Printf("a: %v, b: %v\n", &a, &b)
 }
